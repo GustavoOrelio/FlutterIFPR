53df30bfae615a41e7bb1ab7a19038e5d7123168
Gustavo Orelio <oreliogustavo@gmail.com>
2022-07-05 22:01:49 -0300

    Listando as tarefas

--- Crud/lib/ListaTarefa.dart
+++ Crud/lib/ListaTarefa.dart
@@ -3,18 +3,30 @@ import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class ListaTarefa extends StatelessWidget {
-  buscarDados() async {
+  Future<List<Map<String, Object?>>> buscarDados() async {
     String caminho = join(await getDatabasesPath(), 'banco.db');
-    Database db = await openDatabase(caminho, onCreate: (db, version) {
-      db.execute('''
+    Database db = await openDatabase(
+      caminho,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute('''
           CREATE TABLE tarefa(
-          id INT PRIMARY KEY,
+          id INTEGER PRIMARY KEY,
           nome TEXT NOT NULL,
           descricao TEXT NOT NULL
           )
         ''');
-      db.execute('INSERT INTO tarefa(nome, descricao) VALUES ()');
-    });
+        db.execute(
+            'INSERT INTO tarefa(nome, descricao) VALUES ("Projeto", "Projeto WEB")');
+        db.execute(
+            'INSERT INTO tarefa(nome, descricao) VALUES ("Apresentação", "Apresentação em grupo")');
+        db.execute(
+            'INSERT INTO tarefa(nome, descricao) VALUES ("Lista", "Lista Exercicio")');
+      },
+    );
+    List<Map<String, Object?>> lista =
+        await db.rawQuery('SELECT * FROM tarefa');
+    return lista;
   }
 
   @override
@@ -30,6 +42,23 @@ class ListaTarefa extends StatelessWidget {
               },
               icon: Icon(Icons.add))
         ],
+      ),
+      body: FutureBuilder(
+        future: buscarDados(),
+        builder: (context, AsyncSnapshot<List<Map<String, Object?>>> dadosFuturo){
+          if(!dadosFuturo.hasData) return CircularProgressIndicator();
+          var listaTarefa = dadosFuturo.data!;
+          return ListView.builder(
+            itemCount: listaTarefa.length,
+            itemBuilder: (context, contador){
+              var tarefa = listaTarefa[contador];
+              return ListTile(
+                title: Text(tarefa['nome'].toString()),
+                subtitle: Text(tarefa['descricao'].toString()),
+              );
+            },
+          );
+        },
       ),
     );
   }
