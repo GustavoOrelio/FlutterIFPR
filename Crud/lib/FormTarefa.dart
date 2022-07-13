31c35f1d676ec7b405faf02cde9f167b1254f8f0
Gustavo Orelio <oreliogustavo@gmail.com>
2022-07-12 22:34:37 -0300

    Fazendo funçoes pra salvar e excluir

--- Crud/lib/FormTarefa.dart
+++ Crud/lib/FormTarefa.dart
@@ -1,12 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class FormTarefa extends StatelessWidget {
+  Future<int> salvar(String nome, String descricao, [int? id]) async {
+    String caminho = join(await getDatabasesPath(), 'tarefa.db');
+    Database banco = await openDatabase(caminho, version: 1);
+
+    String sql;
+    Future<int> linhasAfetadas;
+    if (id == null) {
+      sql = 'INSERT INTO tarefa (nome, descricao) VALUES (?, ?)';
+      linhasAfetadas = banco.rawInsert(sql, [nome, descricao]);
+    } else {
+      sql = 'UPDATE tarefa SET nome = ?, descricao = ? WHERE id = ?';
+      linhasAfetadas = banco.rawUpdate(sql, [nome, descricao, id]);
+    }
+    return linhasAfetadas;
+  }
+
+  Future<int> deletar(int id) async {
+    String caminho = join(await getDatabasesPath(), 'tarefa.db');
+    Database banco = await openDatabase(caminho, version: 1);
+    String sql = "DELETE FROM tarefa WHERE id = ?";
+    Future<int> linhaAfetada;
+    linhaAfetada = banco.rawDelete(sql, [id]);
+    return linhaAfetada;
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
+    String? nome;
+    String? descricao;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black87,
         title: const Text('Cadastro de tarefas'),
+      ),
+      body: Column(
+        children: [
+          TextFormField(
+            decoration: InputDecoration(
+                label: Text('Nome'), hintText: 'Informe o nome da tarefa'),
+            onChanged: (valorDigitado) => nome = valorDigitado,
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+                label: Text('Descrição'), hintText: 'Informe a descrição da tarefa'),
+            onChanged: (valorDigitado) => descricao = valorDigitado,
+          ),
+
+          FlatButton(
+            color: Colors.black87,
+            textColor: Colors.white,
+            child: Text('Salvar'),
+            onPressed: () {
+                salvar(nome!, descricao!);
+                Navigator.pushNamed(context, '/');
+
+               //Para salvar precisa pegar o que foi preenchido nos dois campos e salvar;
+            },
+          ),
+
+
+        ],
       ),
     );
   }
--- Crud/lib/ListaTarefa.dart
+++ Crud/lib/ListaTarefa.dart
@@ -1,8 +1,10 @@
+import 'package:crud/FormTarefa.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
 class ListaTarefa extends StatelessWidget {
+
   Future<List<Map<String, Object?>>> buscarDados() async {
     String caminho = join(await getDatabasesPath(), 'tarefa.db');
     Database db = await openDatabase(
@@ -16,12 +18,6 @@ class ListaTarefa extends StatelessWidget {
           descricao TEXT NOT NULL
           )
         ''');
-        db.execute(
-            'INSERT INTO tarefa(nome, descricao) VALUES ("Projeto", "Projeto WEB")');
-        db.execute(
-            'INSERT INTO tarefa(nome, descricao) VALUES ("Apresentação", "Apresentação em grupo")');
-        db.execute(
-            'INSERT INTO tarefa(nome, descricao) VALUES ("Lista", "Lista Exercicio")');
       },
     );
     List<Map<String, Object?>> lista =
@@ -29,15 +25,18 @@ class ListaTarefa extends StatelessWidget {
     return lista;
   }
 
+
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black87,
         title: const Text('Lista de tarefas'),
         actions: [
           IconButton(
-            padding: EdgeInsets.only(left: 15),
+              padding: EdgeInsets.only(left: 15),
               onPressed: () {
                 Navigator.pushNamed(context, '/formTarefa');
               },
@@ -51,16 +50,60 @@ class ListaTarefa extends StatelessWidget {
       ),
       body: FutureBuilder(
         future: buscarDados(),
-        builder: (context, AsyncSnapshot<List<Map<String, Object?>>> dadosFuturo){
-          if(!dadosFuturo.hasData) return CircularProgressIndicator();
+        builder:
+            (context, AsyncSnapshot<List<Map<String, Object?>>> dadosFuturo) {
+          if (!dadosFuturo.hasData) return CircularProgressIndicator();
           var listaTarefa = dadosFuturo.data!;
           return ListView.builder(
             itemCount: listaTarefa.length,
-            itemBuilder: (context, contador){
+            itemBuilder: (context, contador) {
               var tarefa = listaTarefa[contador];
               return ListTile(
                 title: Text(tarefa['nome'].toString()),
                 subtitle: Text(tarefa['descricao'].toString()),
+                trailing: Container(
+                  width: 100,
+                  child: Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.edit),
+                        color: Colors.green,
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  content: Text("Tem certeza que deseja excluir? Obs. Está ação não pode ser desfeita"),
+                                  actions: [
+                                    ElevatedButton(
+                                      child: Text("Sim"),
+                                      onPressed: () {
+                                        FormTarefa form = new FormTarefa();
+                                        form.deletar(
+                                            int.parse(tarefa['id'].toString()));
+                                        Navigator.pushNamed(context, '/');
+                                      },
+                                    ),
+                                    ElevatedButton(
+                                      child: Text("Não"),
+                                      onPressed: () {
+                                        Navigator.pushNamed(context, '/');
+                                      },
+
+                                    ),
+                                  ],
+                                );
+                              });
+                        },
+                        icon: Icon(Icons.delete),
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                ),
               );
             },
           );
