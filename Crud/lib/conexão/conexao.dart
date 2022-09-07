92fdd8c787c9bbc8da0439247ddddd152a651d26
Gustavo Orelio <oreliogustavo@gmail.com>
2022-09-06 23:25:08 -0300

    Adicionando conexão com banco

---
+++ Crud/lib/conexão/conexao.dart
@@ -0,0 +1,23 @@
+import 'package:crud/conexão/script.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class Conexao{
+  static Database? _bancoDeDados;
+
+  static Future<Database> abrir() async{
+    if (_bancoDeDados == null){
+      String caminho = join(await getDatabasesPath(), 'tarefa.db');
+      _bancoDeDados = await openDatabase(
+        caminho,
+        version: 1,
+        onCreate: (db, version){
+          for (var comando in criandoBanco){
+            db.execute(comando);
+          }
+        },
+      );
+    }
+    return _bancoDeDados!;
+  }
+}
